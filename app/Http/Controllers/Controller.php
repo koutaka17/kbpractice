9e7e189bdc3afe860d533a1c789bfc4ede977902
koutaka17 <keryk.kstudyandplay25251515tkkt@gmail.com>
2024-09-22 14:31:22 +0900

    Controller&Modelのインデックスへの追加

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,8 +5,13 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use App\Models\Post;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, ValidatesRequests;
+    public function index(Post $post)
+{
+    return $post->get();
+}
+    
 }
